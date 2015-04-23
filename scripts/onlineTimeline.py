901f7e7787bbaef3404705072e0244081e28e4bb
pbechon <pbechon@961dae45-e315-455c-aeb6-d211cecba20e>
2015-04-23 13:36:40 +0000

    Ajout d'une barre de progression du temps et des échéances de com.

--- scripts/onlineTimeline.py
+++ scripts/onlineTimeline.py
@@ -8,83 +8,129 @@ from plotWindow import PlotWindow
 
 import rospy
 from roshidden.msg import StnVisu
+from std_msgs.msg import Empty
 
+import functools
 import itertools
 import json
+import time
 import sys, random
 
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
 class OnlineTimeline(PlotWindow):
-  def __init__(self):
-    PlotWindow.__init__(self)
+    def __init__(self):
+        PlotWindow.__init__(self)
 
-    self.window_size=20
+        self.window_size=20
+        
+        self.data = {}
+        self.colors = itertools.cycle(["r","g","b"])
+
+        self.mutex = True
+        
+        self.beginDate = time.time()
+
+        rospy.init_node('visu', anonymous=True)
+        self.subscriber = rospy.Subscriber("/hidden/stnvisu", StnVisu, self.plotResults, queue_size = 1 )
+
+        self.subscriber = rospy.Subscriber("/hidden/start", Empty, self.start, queue_size = 1 )
+        
+        rospy.logwarn("Init done for onlineTimeline")
+
+    def start(self, m):
+        self.beginDate = time.time()
+
+    def getCurrentTime(self):
+        if self.beginDate < 0:
+          logger.error("getCurrentTime called before initialisation")
+
+        return int(round(1000 * (time.time() - self.beginDate)))
     
-    self.data = {}
-    self.colors = itertools.cycle(["r","g","b"])
-
-    self.mutex = True
-
-    rospy.init_node('visu', anonymous=True)
-    self.subscriber = rospy.Subscriber("/hidden/stnvisu", StnVisu, self.plotResults, queue_size = 1 )
-
-    rospy.logwarn("Init done for onlineTimeline")
-
-  def plotResults(self, data): 
-    if data.agent not in self.data:
-        self.data[data.agent] = {"color" : next(self.colors), "index":1+len(self.data)}
-
-    self.data[data.agent]["actions"] = []
-    for m in data.actions:
-        self.data[data.agent]["actions"].append({"name":m.name, "timeStart": m.timeStartLb,  "timeEnd": m.timeEndLb, "executed":m.executed, "executing":m.executing, "hierarchical":m.hierarchical})
-
-    rospy.logwarn("received message from %s" % data.agent)
-
-    while not self.mutex:
-      rospy.sleep(0.1)
-    self.mutex = False
+    def getPrintName(self, name):
+        if "move" in name:
+            locs = name.split(" ")[2:]
+            #rospy.logwarn(locs)
+            locs = map(lambda x: "_".join(x.split("_")[1:3]), locs)
+            return "->".join(locs)
+        elif "communicate" in name:
+            return " ".join(name.split(" ")[1:3])
+        else:
+            return name
     
-    self.axes.clear()        
-    self.axes.set_autoscaley_on(False)
-    
-    xmax = max([a["timeEnd"] for d in self.data.values() for a in d["actions"]])
-    self.axes.set_xlim(0, xmax)
-    
-    captions = [" " for _ in range(len(self.data)+2)]
-    for robot in sorted(self.data.keys()):
-        posCycle = itertools.cycle([0.1,0.2,0.3,0.4,0.5])
-        for action in sorted(self.data[robot]["actions"], key=lambda x: x["timeStart"]):
+    def plotResults(self, data): 
+        if data is not None:
+            if data.agent not in self.data:
+                self.data[data.agent] = {"color" : next(self.colors), "index":1+len(self.data)}
 
-            if "has-communicated" in action["name"] or "observe" in action["name"] or action["hierarchical"]:
-                continue
+            self.data[data.agent]["actions"] = []
+            for m in data.actions:
+                self.data[data.agent]["actions"].append({"name":m.name, "timeStart": m.timeStartLb,  "timeEnd": m.timeEndLb, "executed":m.executed, "executing":m.executing, "hierarchical":m.hierarchical})
 
-            index = self.data[robot]["index"]
-            xstart = action["timeStart"]
-            xstop  = action["timeEnd"]
-            if action["executed"]:
-              alpha = 0.6
-            elif action["executing"]:
-              alpha = 1
-            else:
-              alpha = 0.2
+            rospy.logwarn("received message from %s" % data.agent)
 
-            offset = next(posCycle)
-            self.axes.text((xstart + xstop)/2, index + offset, action["name"],horizontalalignment='center',verticalalignment='center',fontsize=8)
-            self.axes.vlines((xstart + xstop)/2, index, index + offset - 0.05)
-            self.axes.axhspan(index-0.05, index+0.05, xstart/xmax, xstop/xmax, facecolor=self.data[robot]["color"], edgecolor="k", lw=1, ls="solid", alpha=alpha)
+        while not self.mutex:
+            rospy.sleep(0.1)
+        self.mutex = False
+        
+        self.axes.clear()        
+        self.axes.set_autoscaley_on(False)
+        
+        if self.data:
+            xmax = max([a["timeEnd"] for d in self.data.values() for a in d["actions"]])
+        else:
+            xmax = 1
+        self.axes.set_xlim(0, xmax)
+        
+        captions = [" " for _ in range(len(self.data)+2)]
+        for robot in sorted(self.data.keys()):
+            posCycle = itertools.cycle([0.1,0.2,0.3,0.4,0.5])
+            for action in sorted(self.data[robot]["actions"], key=lambda x: x["timeStart"]):
+
+                if "observe" in action["name"] or action["hierarchical"]:
+                    continue
+                    
+                if "has-communicated" in action["name"]:
+                    self.axes.vlines(action["timeStart"], index-0.1, index+0.1, color="m", lw=5)
+                    
+                    continue
+
+                index = self.data[robot]["index"]
+                xstart = action["timeStart"]
+                xstop  = action["timeEnd"]
+                if action["executed"]:
+                  alpha = 0.6
+                elif action["executing"]:
+                  alpha = 1
+                else:
+                  alpha = 0.2
+
+                color = self.data[robot]["color"]
+
+                offset = next(posCycle)
+                self.axes.text((xstart + xstop)/2, index + offset, self.getPrintName(action["name"]),horizontalalignment='center',verticalalignment='center',fontsize=8)
+                self.axes.vlines((xstart + xstop)/2, index, index + offset - 0.05)
+                self.axes.axhspan(index-0.05, index+0.05, xstart/xmax, xstop/xmax, facecolor=color, edgecolor="k", lw=1, ls="solid", alpha=alpha)
+                
             captions[index] = robot
 
-    self.axes.set_yticks(range(len(captions)))
-    self.axes.set_yticklabels(captions)
+        self.axes.set_yticks(range(len(captions)))
+        self.axes.set_yticklabels(captions)
 
-    self.canvas.draw()
-    
-    self.mutex = True
+        self.axes.vlines(self.getCurrentTime(), 0, len(captions), color="r")
+                
+        self.canvas.draw()
+        
+        self.mutex = True
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = OnlineTimeline()
     window.show()
+    
+    timer = QTimer()
+    timer.timeout.connect(functools.partial(window.plotResults, None))
+    timer.start(1000)
+    
     app.exec_()
