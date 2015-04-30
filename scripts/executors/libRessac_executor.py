31380cdfcdf7f18da300f1df204f2aea9e3c6395
pchavent <pchavent@961dae45-e315-455c-aeb6-d211cecba20e>
2015-04-30 10:25:35 +0000

    Fix libRessac_executor.

--- scripts/executors/libRessac_executor.py
+++ scripts/executors/libRessac_executor.py
@@ -1,94 +1,126 @@
-from .action_executor import DummyActionExecutor
+from .action_executor import AbstractActionExecutor
 
 import logging; logger = logging.getLogger("hidden")
 
 try:
     import libRessac
 
-    # En simu
-    #TTY="/tmp/libRessac-Simu-26-to-payload.pty"
-    # Sur la cible
-    TTY="/dev/ttyS0"
-    
+    # Simulation : connect to pty and automatic route launch
+    TTY="/tmp/libRessac-Simu-26-to-payload.pty"
+    PHASE_ROUTE=libRessac.PhaseRoute
+    # Otherwise : connect to real device and wait operator for route triggering
+    #TTY="/dev/ttyS0"
+    #PHASE_ROUTE=libRessac.PhaseHovering
+
     ALT=40
     SPD=3
     DIST=6
-    
-    class libRessacExecutor(DummyActionExecutor):
+
+    class libRessacExecutor(AbstractActionExecutor):
         _name = "libRessac"
-    
+
+        #
+        # Move states :
+        #
+        class MoveState:
+            def __init__(self, x, y):
+                self.state = "initial"
+                self.dst_x = x
+                self.dst_y = y
+                self.wp_id = -1
+                self.mi_id = -1
+
+            def isFinal(self):
+                if (self.state == "final"):
+                    return 1
+                else:
+                    return 0
+
+        #
+        # ctor
+        #
         def __init__(self, agentName="ressac1", **kwargs):
-            DummyActionExecutor.__init__(self, agentName)
-    
+
             logger.info("libRessac executor : ctor")
-    
+
+            # agent name and id
             self.agent = agentName
             if(self.agent == "ressac1"):
                 self.id = 23
             else:
                 self.id = 26
+
             logger.info("libRessac executor : agent %s (%d)" % (self.agent, self.id))
-    
-            self.pendingEvents = [] # list of dictionnary of pending events
-    
+
+            # list of dictionnary of pending events
+            self.pendingEvents = []
+
+            # libRessac specific init
             self.libRessacStart()
-    
+
+        #
+        # dtor
+        #
         def __del__(self):
+
             logger.info("libRessac executor : dtor")
+
+            # libRessac specific termination
             self.libRessacStop()
-    
+
+        #
+        # periodic update
+        #
         def update(self):
-            DummyActionExecutor.update(self)
+
             #logger.info("libRessac executor : update")
+
             for pe in self.pendingEvents:
-                x = pe["x"]
-                y = pe["y"]
-                wp_id = pe["wp_id"]
-                res = self.libRessacGotoIsDone(x, y, wp_id)
-                if(res != "pending"):
-                    logger.info("libRessac executor : goto (%d;%d) %s" % (x, y, res))
+                pe["state"] = pe["update"](pe["state"])
+                if (pe["state"].isFinal()):
                     pe["cb"]("ok")
                     self.pendingEvents.remove(pe)
-    
-        
+
+
+        #
+        # move implementation
+        #
         def move(self, who, a, b, cb, **kwargs):
-    
+
             # check agent
-        if(self.agent is not None and self.agent != who):
-            logger.warning("libRessac executor : ignore action %s for %s" % (actionJson["name"], self.agent))
-            pass
+            if(self.agent is not None and self.agent != who):
+                logger.warning("libRessac executor : ignore action %s for %s" % (actionJson["name"], self.agent))
+                pass
 
-        # decode coords : ACTION convention ENU -> NED 
-        coords = b.split("_")[1:]
-        x = int(int(coords[1])/100)
-        y = int(int(coords[0])/100)
-        logger.info("libRessac executor : goto (%d;%d)" % (x, y))
+            # decode coords : ACTION convention ENU -> libRessac convention NED
+            coords = b.split("_")[1:]
+            x = int(int(coords[1])/100)
+            y = int(int(coords[0])/100)
 
-        # check is already
-        res = self.libRessacGotoIsDone(x, y, -1)
-        if(res == "ok"):
-            cb("ok")
-        else:
-            # start the action
-            wp_id = self.libRessacGoto(x, y)
-            self.pendingEvents.append( {"x":x, "y":y, "wp_id":wp_id, "cb": cb} )
-    
+            logger.info("libRessac executor : move (%d;%d)" % (x, y))
+
+            # append the event
+            self.pendingEvents.append( {"update":self.libRessacMove, "state":self.MoveState(x, y), "cb": cb} )
+
+        #
+        # libRessac specific start
+        #
         def libRessacStart(self):
-    
+
             # System manager instance
             self.system_manager = libRessac.SystemManager()
-            
+
             # Init proxy
             self.proxy = libRessac.ProxySystem()
             self.proxy.setSystemManager(self.system_manager)
             self.proxy.setId(self.id)
             self.system_manager.updateSystem(self.id, self.proxy, 1);
-            
+
             # Link stuff
             self.tty = libRessac.SerialTty()
             self.pps = libRessac.SerialPps()
             self.log = libRessac.FileIo()
-            
+
             # Init link
             self.link = libRessac.Link()
             self.link.setDataInterface(self.tty)
@@ -97,122 +129,155 @@ try:
             self.link.setSystemManager(self.system_manager)
             self.link.setId(libRessac.SystemIdPayload)
             self.system_manager.updateLinkTx(self.id, self.link)
-             
+
             # Configure tty
-            self.tty.setPath(TTY) 
+            self.tty.setPath(TTY)
             self.tty.setBaud(115200)
             self.tty.setData(8)
             self.tty.setParity('N')
             self.tty.setStop(1)
             self.tty.setVtime(1)
-            self.tty.setVmin(0xff) 
+            self.tty.setVmin(0xff)
             self.tty.txEnable(1)
-            
+
             # Configure pps
             self.pps.setPath(TTY)
             self.pps.setMaster(0)
-            
+
             # Configure logger
             self.log.setLogger(1)
             self.log.setFileName(TTY)
-            
+
             # Create a link thread
             self.link_thread = libRessac.Thread(self.link)
             self.link_thread.setPriority(80)
             self.link_thread.setName("link-tty")
-        
+
             # Start thread
             self.link_thread.initialize()
             self.link_thread.startup()
-    
+
             # Get uav from system manager
             self.uav = self.system_manager.getSystem(self.id)
-    
+
+        #
+        # libRessac specific stop
+        #
         def libRessacStop(self):
-    
+
             # Stop thread
             self.link_thread.shutdown()
             self.link_thread.finalize()
-    
-        def libRessacGoto(self, x, y):
-    
-            waypoint = libRessac.Waypoint()
-            waypoint.id = libRessac.WaypointIdPayloadMin
-            waypoint.flags = libRessac.DataFlagReady
-            waypoint.x = x
-            waypoint.y = y
-            waypoint.h = ALT
-            waypoint.psi = 0
-            waypoint.beta = 0
-            waypoint.speed = SPD
-            waypoint.dist = DIST
-            waypoint.phase_rt = libRessac.PhaseRoute
-            waypoint.phase_wp = libRessac.PhaseHoveringOnTarget
-            waypoint.wait = 0
-            self.uav.setWaypoint(waypoint)
-            print(waypoint)
-    
-            route = libRessac.Route()
-            route.id = libRessac.RouteIdPayloadMin
-            route.flags = libRessac.DataFlagReady
-            route.nb_wps = 1
-            route.nb_repeat = 0
-            route.wps[0] = libRessac.WaypointIdPayloadMin
-            self.uav.setRoute(route)
-            print(route)
-        
-            mission = libRessac.Mission()
-            mission.id = libRessac.MissionIdPayloadMin
-            mission.flags = libRessac.DataFlagReady
-            mission.id_route = libRessac.RouteIdPayloadMin
-            #mission.acked = 0
-            mission.acked = 1
-            mission.status = 0
-            self.uav.setMission(mission)
-            print(mission)
 
-            phase = libRessac.Phase()
-            phase.phase = libRessac.PhaseHovering
-            phase.cons_type = libRessac.ConsTypeMission
-            phase.cons.mission.mission_id = libRessac.MissionIdPayloadMin
-            phase.cons.mission.waypoint_id = 0
-            self.uav.setPhase(phase)
-            print(phase)
-            
-            logger.info("libRessac executor : set phase ")
-    
-            while True:
-                state_basic=libRessac.StateBasic()
-                self.uav.getStateBasic(state_basic)
-                if (state_basic.phase == libRessac.PhaseRoute):
-                    break
-    
-            logger.info("libRessac executor : set phase done")
-    
-            return waypoint.id
-        
-        def libRessacGotoIsDone(self, x, y, wp_id):
-    
-            logger.info("libRessac executor : goto is done ?")
-    
-            state_basic=libRessac.StateBasic()
+        #
+        # libRessac specific move
+        #
+        # initial -> ready -> ongoing -> reached -> final
+        #                             -> error
+        def libRessacMove(self, move):
+
+            # get current avionic state
+            state_basic = libRessac.StateBasic()
             self.uav.getStateBasic(state_basic)
-        
-            dtg = (state_basic.x - x)**2 + (state_basic.y - y)**2 
-    
-            if (state_basic.phase == libRessac.PhaseHoveringOnTarget) or (dtg < DIST**2) or ((wp_id != -1) and (state_basic.phase == libRessac.PhaseHovering)):
-                phase = libRessac.Phase()
-                phase.phase = libRessac.PhaseHovering
-                phase.cons_type = libRessac.ConsTypeUndefined
-                self.uav.setPhase(phase)
-                logger.info("libRessac executor : goto done")
-                return "ok"
-            elif (state_basic.phase == libRessac.PhaseRoute) and (state_basic.wp_current == wp_id):
-                return "pending"
-            #elif (state_basic.phase == libRessac.PhaseHovering):
-            #    return "interrupted"
-            else:
-                return "unexpected"
+
+            # compute distance to go to next wp
+            dtg = (state_basic.x - move.dst_x)**2 + (state_basic.y - move.dst_y)**2
+
+            if (move.state == "initial"):
+
+                if (dtg < DIST**2) and \
+                   ((state_basic.phase == libRessac.PhaseHovering) or \
+                    (state_basic.phase == libRessac.PhaseHoveringOnTarget)):
+
+                    move.state = "final"
+
+                else:
+
+                    logger.info("libRessac executor : exec move")
+
+                    waypoint = libRessac.Waypoint()
+                    waypoint.id = libRessac.WaypointIdPayloadMin
+                    waypoint.flags = libRessac.DataFlagReady
+                    waypoint.x = move.dst_x
+                    waypoint.y = move.dst_y
+                    waypoint.h = ALT
+                    waypoint.psi = 0
+                    waypoint.beta = 0
+                    waypoint.speed = SPD
+                    waypoint.dist = DIST
+                    waypoint.phase_rt = libRessac.PhaseRoute
+                    waypoint.phase_wp = libRessac.PhaseHoveringOnTarget
+                    waypoint.wait = 0
+                    self.uav.setWaypoint(waypoint)
+                    print(waypoint)
+
+                    route = libRessac.Route()
+                    route.id = libRessac.RouteIdPayloadMin
+                    route.flags = libRessac.DataFlagReady
+                    route.nb_wps = 1
+                    route.nb_repeat = 0
+                    route.wps[0] = waypoint.id
+                    self.uav.setRoute(route)
+                    print(route)
+
+                    mission = libRessac.Mission()
+                    mission.id = libRessac.MissionIdPoolQuery
+                    mission.flags = libRessac.DataFlagReady
+                    mission.id_route = route.id
+                    mission.acked = 1
+                    mission.status = 0
+                    self.uav.getMission(mission)
+                    print(mission)
+
+                    phase = libRessac.Phase()
+                    phase.phase = PHASE_ROUTE
+                    phase.cons_type = libRessac.ConsTypeMission
+                    phase.cons.mission.mission_id = mission.id
+                    phase.cons.mission.waypoint_id = 0
+                    self.uav.setPhase(phase)
+                    print(phase)
+
+                    move.wp_id = waypoint.id
+                    move.mi_id = mission.id
+                    move.state = "ready"
+
+            elif (move.state == "ready"):
+
+                if (state_basic.phase == libRessac.PhaseRoute) and (state_basic.wp_current == move.wp_id):
+
+                    move.state = "ongoing"
+
+                elif ((state_basic.phase != libRessac.PhaseHovering) and \
+                      (state_basic.phase != libRessac.PhaseHoveringOnTarget)):
+
+                    move.state = "error"
+
+            elif (move.state == "ongoing"):
+
+                if (dtg < DIST**2) and \
+                   ((state_basic.phase == libRessac.PhaseHovering) or \
+                    (state_basic.phase == libRessac.PhaseHoveringOnTarget)):
+
+                    move.state = "reached"
+
+                elif (state_basic.phase != libRessac.PhaseHovering) and \
+                     (state_basic.phase != libRessac.PhaseHoveringOnTarget) and \
+                     ((state_basic.phase != libRessac.PhaseRoute) or (state_basic.wp_current != move.wp_id)) :
+
+                    move.state = "error"
+
+            elif (move.state == "reached") or (move.state == "error"):
+
+                mission = libRessac.Mission()
+                mission.id = move.mi_id
+                mission.flags = libRessac.DataFlagFree
+                self.uav.setMission(mission)
+
+                move.state = "final"
+
+            logger.info("libRessac executor : move state : %s" % move.state)
+
+            return move
 
 except ImportError:
     pass
