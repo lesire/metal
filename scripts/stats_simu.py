70a9c48351cd5c1b4ceebb0f0b8ce8669d883f1f
Patrick Bechon <patrick.bechon@onera.fr>
2015-05-29 19:52:20 +0200

    Add the parsing for the simulation. Add options to launch several simulation in succession

--- scripts/stats_simu.py
+++ scripts/stats_simu.py
@@ -1,15 +1,22 @@
-#! /usr/bin/env python3
+#! /usr/bin/env python
+### rosbag API is not python3 compatible ###
 
+import argparse
+import json
 import logging; logger = logging.getLogger("simu")
 import os
+import re
 import shutil
 import subprocess
+import sys
 import time
 
-import rospy
 import rospkg
 rospack = rospkg.RosPack()
-from std_msgs.msg import Empty
+import rosbag
+from std_msgs.msg import Empty, String
+from metal.msg import StnVisu
+
 
 sh = logging.StreamHandler()
 logger.setLevel(logging.INFO)
@@ -20,8 +27,8 @@ logger.addHandler(sh)
 class InputError(Exception):
     pass
 
-def getNewOutputDir():
-    return os.path.join(rospack.get_path("metal"), "data/simu_output/" + time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime()))
+def getNewOutputDir(prefix = "simu"):
+    return os.path.join(rospack.get_path("metal"), "data/simu_output/" + prefix + "_" + time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime()))
 
 def runSimu(missionDir, aleaFile, outputDir = None):
     # Sanity check
@@ -58,7 +65,7 @@ def runSimu(missionDir, aleaFile, outputDir = None):
 
     #run the rolaunch command
     os.environ["ROS_LOG_DIR"] = outputDir
-    p = subprocess.Popen("roslaunch --run_id=roslaunch stats_simu.launch alea_file:={alea} visu:=true".format(alea=aleaFile).split(" "))
+    p = subprocess.Popen("roslaunch --run_id=roslaunch stats_simu.launch alea_file:={alea}".format(alea=aleaFile).split(" "))
 
     time.sleep(5)
 
@@ -67,13 +74,120 @@ def runSimu(missionDir, aleaFile, outputDir = None):
     #TODO add a timeout or make the watcher detect deadlocks
     p.wait()
 
-    #TODO parse the output
-
+    parseSimu(outputDir)
+    
     logger.info("done with this simulation")
 
-def main():
+"""
+Parse the ros bag and extract metrics :
+ - Success of the mission
+ - Number of points explored
+ - Duration of the mission
+"""
+def parseSimu(outputDir):
+    logger.info("Parsing %s" % outputDir)
+    result = {"obsPoints" : {}}
+    
+    ## Parse the pddl files ##
+    
+    pddlPrb = None
+    for f in os.listdir(os.path.join(outputDir, "hipop-files")):
+        if re.match(".*-prb.pddl", f):
+            pddlPrb = f
+            break #assume there is only one file for this pattern
 
-    runSimu("/Users/patrick/Documents/workspace_planner/ressources/missions/odas-mission", "/Users/patrick/Documents/workspace_planner/action_ros_ws/src/metal/data/delay-odas.json")
+    with open(os.path.join(outputDir, "hipop-files", pddlPrb)) as f:
+        strPrb = " ".join(f.readlines())
+        #TODO : replace("\n", " ")
+
+    obsPointsNominal = re.findall("(explored [^)]*)", strPrb)
+    result["obsPoints"]["nominalNbr"] = len(obsPointsNominal)
+
+    ## Parse the ros bag ##
+    
+    bag = rosbag.Bag(os.path.join(outputDir, "stats.bag"))
+    
+    try:
+        logger.info("Number of messages %s" % bag.get_message_count())
+        
+        obsPoints = set()
+        for _, msg, _ in bag.read_messages(topics="/hidden/stats"):
+            data = json.loads(msg.data)
+            
+            if data["type"] == "observe":
+                obsPoints.add(data["to"])
+            
+            logger.info(data)
+
+        result["obsPoints"]["nbr"] = len(obsPoints)
+        result["obsPoints"]["ratio"] = float(result["obsPoints"]["nbr"]) / result["obsPoints"]["nominalNbr"]
+
+        hasError = False
+        agents = {}
+        for _,msg,_ in bag.read_messages(topics="/hidden/stnvisu"):
+            if msg.agent not in agents:
+                agents[msg.agent] = {}
+            
+            if "finishTime" not in agents[msg.agent] and msg.state in ["DONE", "ERROR", "DEAD"]:
+                agents[msg.agent]["finishTime"] = msg.time
+                
+            if msg.state == "ERROR":
+                hasError = True
+            
+            logger.debug("%s : %s -> %s" % (msg.time, msg.agent, msg.state))
+
+        for agent,d in agents.items():
+            if "finishTime" not in d:
+                logger.error("Cannot determine the end time of %s" % agent)
+
+        result["success"] = not hasError
+        result["finishTime"] = max([d["finishTime"]/1000. for d in agents.values() if "finishTime" in d])
+        
+    finally:
+        bag.close()
+
+    with open(os.path.join(outputDir, "results.json"), "w") as f:
+        json.dump(result, f, indent=2)
+
+def main(argv):
+    parser = argparse.ArgumentParser(description="Launch a statistical simulation")
+    parser.add_argument("-a", "--aleaFiles", type=os.path.abspath, nargs="+")
+    parser.add_argument("-m", "--mission"  , type=os.path.abspath)
+    parser.add_argument("--parseOnly"      , action="store_true")
+    parser.add_argument("--outputFolder"   , type=os.path.abspath)
+    parser.add_argument("--logLevel"       , type=str, default="info")
+    args = parser.parse_args(argv)
+
+    ## Parse only ##
+    if args.parseOnly:
+        if args.outputFolder is None:
+            logger.error("Cannot parse. No given output folder")
+            sys.exit(1)
+        if not os.path.exists(args.outputFolder):
+            logger.error("Cannot open the output folder : %s" % args.outputFolder)
+            sys.exit(1)
+    
+        parseSimu(args.outputFolder)
+        sys.exit(0)
+
+    ## Run simulation ##
+    if args.aleaFiles is None or args.mission is None:
+        logger.error("You must specify at least one alea file and the mission folder")
+        sys.exit(1)
+
+    if not os.path.isdir(args.mission):
+        logger.error("The mission folder does not exists : %s" % args.mission)
+        sys.exit(1)
+
+    for alea in args.aleaFiles:
+        if not os.access(alea, os.R_OK):
+            logger.error("Cannot open a given alea file : %s" % alea)
+            sys.exit(1)
+
+    for alea in args.aleaFiles:
+        runSimu(args.mission, alea)
+
+    #runSimu("$ACTION_HOME/ressources/missions/odas-mission", "$ACTION_HOME/action_ros_ws/src/metal/data/delay-odas.json")
 
 if __name__=="__main__":
-    main()
+    main(sys.argv[1:])
