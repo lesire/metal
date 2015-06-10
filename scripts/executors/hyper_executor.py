0a9ef15d3cbe6927d2330c396a6dc209382a6aa1
ACTION - PC mission <action@onera.fr>
2015-06-10 16:44:42 +0200

    Retry after a blocked report for hyper

--- scripts/executors/hyper_executor.py
+++ scripts/executors/hyper_executor.py
@@ -1,5 +1,6 @@
 import logging; logger = logging.getLogger("hidden")
 import re
+import json
 
 try:
     from .ros_executor import ROSActionExecutor
@@ -14,22 +15,30 @@ try:
             self.output_port.open("/" + agentName + "/executor/out")
             self.input_port = yarp.BufferedPortBottle()
             self.input_port.open("/" + agentName + "/executor/in")
+
+            self.currentAction = None
+            self.isTracking = False
+            self.hasSeenTarget = False
+
             ROSActionExecutor.__init__(self, agentName)
 
         def __del__(self):
             self.output_port.close()
             self.input_port.close()
 
-        # move mana agv_pt_-726_2375 agv_pt_3323_175
-        def move(self, who, a, b, cb, **kwargs):
-            import json
-            coords = b.split("_")[1:]
-            logger.info("moving {w} from {a} to {b}".format(w=who,a=a,b=str(coords)))
-            action = {'action': 'goto', 'waypoint': {'x': int(coords[0])/100, 'y': int(coords[1])/100}}
+        def _sendCommand(self, action):
             b = self.output_port.prepare()
             b.clear()
             b.addString(json.dumps(action))
             self.output_port.write()
+
+        # move mana agv_pt_-726_2375 agv_pt_3323_175
+        def move(self, who, a, b, cb, **kwargs):
+
+            coords = b.split("_")[1:]
+            logger.info("moving {w} from {a} to {b}".format(w=who,a=a,b=str(coords)))
+            action = {'action': 'goto', 'waypoint': {'x': int(coords[0])/100, 'y': int(coords[1])/100}}
+            self._sendCommand(action)            
             logger.info("Request action " + json.dumps(action))
             self._cb = cb
 
@@ -40,26 +49,36 @@ try:
         def update(self):
             ROSActionExecutor.update(self)
             try:
-                import json
                 b = self.input_port.read(False)
                 if b is not None:
                     logger.info("Received report " + b.toString())
                     r = json.loads(b.get(0).toString())
                     r["type"] = r["report"]
+
+                    if r["type"] == "blocked":
+                        self._sendCommand(action)
+                        return
+                    elif "target" in r["type"]:
+                        self.hasSeenTarget = True
                     self._cb(r)
             except Exception as e:
                 logger.warning(e)
 
-        def track(self, *args, **kwargs):
-            import json
-            logger.info("tracking")
-            action = {'action': 'track'}
-            b = self.output_port.prepare()
-            b.clear()
-            b.addString(json.dumps(action))
-            self.output_port.write()
-            logger.info("Request action " + json.dumps(action))
+        def track(self, x, y, *args, **kwargs):
+            self.isTracking = True
             self._cb = cb
+            if not self.hasSeenTarget:
+                #TODO goto_target and track ?
+                logger.info("moving to target".format(w=who,a=a,b=str(coords)))
+                action = {'action': 'goto_target', 'waypoint': {'x': int(x)/100, 'y': int(y)/100}}
+                self._sendCommand(action)            
+                logger.info("Request action " + json.dumps(action))
+            else:
+                logger.info("tracking")
+                action = {'action': 'track'}
+                self._sendCommand(action)
+                logger.info("Request action " + json.dumps(action))
+
 
 except ImportError:
     pass
